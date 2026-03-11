38f46d70cda643802a1c31a1641f86aada759755
Your Name <you@example.com>
2026-03-12 01:18:42 +0200

    unfold admin integration

--- category/admin.py
+++ category/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import Category
 
-admin.site.register(Category)
+@admin.register(Category)
+class CategoryAdmin(ModelAdmin):
+    list_display = ['name', 'image']
+    search_fields = ['name']
--- product/admin.py
+++ product/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
+from unfold.admin import ModelAdmin
 from .models import Product
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(ModelAdmin):
+    list_display = ['name', 'price', 'is_active', 'category']
+    search_fields = ['name', 'price', 'is_active', 'category'] 
+    list_filter = ['price', 'is_active', 'category']
