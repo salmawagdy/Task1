680c1b442c1c79820f56a52d561ade2b947acf87
Your Name <you@example.com>
2026-03-11 04:01:54 +0200

    create data models

---
+++ product/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from category.models import Category
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
