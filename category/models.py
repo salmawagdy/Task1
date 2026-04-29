7f2f55fc64c29e7eca0b6db551c77214dc8c4554
Your Name <you@example.com>
2026-04-29 23:05:28 +0300

    soft delete

--- category/models.py
+++ category/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=100)
     image = models.ImageField(upload_to='category_images/')
+    created_at = models.DateTimeField(auto_now_add=True)
 
 
     class Meta:
--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import product_list, active_products, create_product
+from .views import product_list, active_products, create_product, product_detail
 
 urlpatterns = [
     path('products/', product_list, name='product-list'),
     path('products/active/', active_products, name='active-products'),
     path('products/create/', create_product, name='create-product'),
+    path('products/detail/<int:pk>/', product_detail, name='product-detail'),
 ]
